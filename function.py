1575bf82263731f2f8454027f3bf3ea25849fc9c
Zuhdi <zuhdiwahyudi73@gmail.com>
2024-07-04 09:19:27 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+#function bisa temu prameter atau argument
+def my_unoication():
+    nama = "wahdani"
+    alamat = "lotim"
+    pekerjaan = "guru"
+    print("nama:",nama)
+    print("alamat:",alamat)
+    print("pekerjaan:", pekerjaan)
+my_unoication()
+
+#
+def myunicaction_parameter(nama,alamat):
+    print("nama:",nama)
+    print("alamat:",alamat)
+
+my_unoication()
+
+myunicaction_parameter("wahida","lotim")
+myunicaction_parameter("zuhdi","setiling")
+print("selesai")
+
+nama = "Desi"
+
+myunicaction_parameter(nama,"setiling")
